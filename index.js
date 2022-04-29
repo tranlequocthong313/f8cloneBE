92cc67c2dbb0a26a49d472d7a8f9d96df9a17446
thongahuhu <thongahuhu@gmail.com>
2022-04-29 19:20:35 +0700

    fix bug

--- index.js
+++ index.js
@@ -8,26 +8,26 @@ const compression = require('compression')
 const createError = require('http-errors')
 const helmet = require('helmet')
 const logEvents = require('./src/helper/logEvents')
-const app = express()
 const socketHandlers = require('./src/helper/socket')
 const session = require('express-session')
+const app = express()
+
 const { createServer } = require('http')
 const { Server } = require('socket.io')
 const httpServer = createServer(app)
-
 const io = new Server(httpServer, {
   cors: {
     origin: `*`,
     methods: ['GET', 'POST'],
   },
 })
-
 socketHandlers(io)
 
-const PORT = process.env.PORT || 5000
+db.connect()
 
+app.use(cors())
+app.use(helmet())
 app.set('trust proxy', 1)
-
 app.use(
   session({
     secret: process.env.ACCESS_SESSION_SECRET,
@@ -39,24 +39,17 @@ app.use(
     },
   })
 )
-
-app.use(cors())
-
-app.use(helmet())
-
 app.use(
   compression({
     level: 6,
     threshold: 100 * 1000,
   })
 )
-
 app.use(
   express.urlencoded({
     extended: true,
   })
 )
-
 app.use(express.json())
 
 route(app)
@@ -73,6 +66,5 @@ app.use((err, req, res, next) => {
   })
 })
 
-db.connect()
-
+const PORT = process.env.PORT || 5000
 httpServer.listen(PORT, () => `Server started on port ${PORT}`)
